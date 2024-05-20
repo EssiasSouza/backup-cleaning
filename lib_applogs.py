1a64ca1622897136fc4b4bd04c5e8186d1d230bf
Essias Souza <essias.souza@fleetcor.com.br>
2024-05-20 17:52:00 -0300

    Execution tested and deployed configuration of log path and name by settings.json

--- lib_applogs.py
+++ lib_applogs.py
@@ -2,23 +2,43 @@ import logging
 from logging.handlers import TimedRotatingFileHandler
 import configparser
 import os
+import json
+
+
+with open('settings.json', 'r') as file:
+    settings = json.load(file)
+
+logs_path = ""
+log_name = ""
+for item in settings:
+    if 'logs_path' in item:
+        logs_path = item['logs_path']
+    if 'log_name' in item:
+        log_name = item['log_name']
+
+if logs_path and log_name:
+    full_log_path = os.path.join(logs_path, log_name)
+    print(f"Full log path: {full_log_path}")
+else:
+    print("logs_path or log_name is missing in the settings.json")
+
+
+
 
 LogFolder = './logs'
 
 print("-- Checking log directory...")
 
 if os.path.exists(LogFolder):
-    print(f"-- The directory {LogFolder} exists.")
+    print(f"-- The directory {logs_path} exists.")
 else:
-    print(f"-- The directory {LogFolder} does not exist.")
+    print(f"-- The directory {logs_path} does not exist.")
     print(f"-- Directory created.")
-    os.mkdir('./logs')
+    os.mkdir(f'./{logs_path}')
 
-# Log name of application
-logFileName = LogFolder + "/backup-cleaning.log"
 
 logger = logging.getLogger()    
-handler = TimedRotatingFileHandler(logFileName,
+handler = TimedRotatingFileHandler(full_log_path,
 when="d",
 interval=1,
 backupCount=7)
