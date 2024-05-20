052526edce22081e499ea8d464bd818bf86b7b6b
Essias Souza <essias.souza@fleetcor.com.br>
2024-05-20 12:20:49 -0300

    Timer deployed and defined data fixed on the loop

--- backup-cleaning.py
+++ backup-cleaning.py
@@ -9,21 +9,19 @@ import json
 print('Starting application')
 lib_applogs.logger.warning('Starting application')
 
-# # TESTING DATE
-# input_date_str = "04/05/2023"
-# current_date = datetime.datetime.strptime(input_date_str, "%d/%m/%Y")
-# formated_date = current_date.strftime("%d/%m/%Y %H:%M:%S")
-
-current_date = datetime.datetime.now()
-formated_date = current_date.strftime("%d/%m/%Y %H:%M:%S")
-
-print("Defined date:", formated_date)
-lib_applogs.logger.warning(f'Defined date:{formated_date}')
-
-
-
 def daily():
     def main():
+
+        # # TESTING DATE
+        # input_date_str = "04/05/2023"
+        # current_date = datetime.datetime.strptime(input_date_str, "%d/%m/%Y")
+        # formated_date = current_date.strftime("%d/%m/%Y %H:%M:%S")
+
+        current_date = datetime.datetime.now()
+        formated_date = current_date.strftime("%d/%m/%Y %H:%M:%S")
+
+        print("Defined date:", formated_date)
+        lib_applogs.logger.warning(f'Defined date:{formated_date}')
         while True:
 
             def delete():
@@ -33,6 +31,7 @@ def daily():
                 for param in params:
                     try:
                         print(f'DIRECTORY: {param["folder_path"]}')
+                        lib_applogs.logger.warning(f'DIRECTORY: {param["folder_path"]}')
 
                         day_less = int(param["day_less"])
 
@@ -55,22 +54,41 @@ def daily():
                                     shutil.rmtree(subfolder)
                                     lib_applogs.logger.warning(f'Deleted: {subfolder}')
                                     time.sleep(2)
+                                else:
+                                    print(f'-- {subfolder} not matches the deletion date')
+                                    lib_applogs.logger.warning(f'{subfolder} found, but not matches the deletion date')
                                     
                         else:
                             time.sleep(2)
-                            print('-- There is nothing to delete')
-
-                        lib_applogs.logger.warning(f'End of purge process.')
+                            print('-- There are no subfolders in this path')
+                            lib_applogs.logger.warning(f'There are no subfolders in this path.')
                     except:
                         time.sleep(2)
-                        print('Path not found')
-            # delete()
+                        print('There are no objects found')
+                        lib_applogs.logger.warning('There is no object found')
+            delete()
+
+            # Calculating time to run again
             with open('settings.json', 'r', encoding='utf-8') as file:
                 settings  = json.load(file)
             run_pause_time_str = settings[0]['run_pause_time']
             run_pause_time = int(run_pause_time_str)
-            hours_calculate = (run_pause_time / 60) / 60
-            finish = f'Waiting for {run_pause_time} seconds ({hours_calculate} hours) to execute again.'
+
+            if run_pause_time < 60:
+                hours = 00
+                minutes = 00
+                seconds = run_pause_time
+            elif run_pause_time < 3600:
+                hours = 00
+                minutes = run_pause_time // 60
+                seconds = run_pause_time % 60
+            else:
+                hours = run_pause_time // 3600
+                remaining_time = run_pause_time % 3600
+                minutes = remaining_time // 60
+                seconds = remaining_time % 60
+
+            finish = f'Waiting for {run_pause_time} seconds ({hours:.0f}h{minutes:.0f}m{seconds:.0f}s) to execute again.'
             print(finish)
             lib_applogs.logger.warning(finish)
             time.sleep(run_pause_time)
